5e10296220d16ff6fb79fa2a16e8c384088d6365
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2024-11-24 17:05:48 +0200

    init project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+          <div className="w-full h-full">
+            <h1 className="text-lg text-indigo-500 w-full h-full text-center"> Angels-Radar </h1>
+          </div>
+  );
+}
