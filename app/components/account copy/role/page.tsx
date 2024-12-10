b4e38b626fe6a681c44d22cb286054b6f65a27c9
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2024-12-10 22:10:57 +0200

    add fix account

---
+++ app/components/account copy/role/page.tsx
@@ -0,0 +1,12 @@
+import ChooseRole from '@/app/components/forms/UserRole'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <ChooseRole />
+    </div>
+  )
+}
+
+export default page
